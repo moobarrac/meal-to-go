16c1b35320882f2f318a952525b40f17eee1446e
moobarrac <wahabajibola01@gmail.com>
2023-05-23 12:50:11 +0100

    card solution

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,22 +1,38 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { Card } from "react-native-paper";
 
-const RestaurantInfo = ({ restaurant }) => {
+const RestaurantInfo = ({ restaurant = {} }) => {
   const {
     name = "This restaurant",
     icon,
     address = "@4, Abari Street, Lagos",
     rating = 4,
     isOpenNow = true,
-    photos = ["../../../../assets/restaurant.jpg"],
+    photos = ["https://picsum.photos/700"],
     isClosedTemporarily = false,
   } = restaurant;
 
   return (
     <View>
-      <Text>Restaurant Info</Text>
+      <Card style={styles.card} elevation={4}>
+        <Card.Cover source={{ uri: photos[0] }} />
+        <Card.Content style={styles.content}>
+          <Text variant="titleLarge">{name}</Text>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 5,
+    backgroundColor: "gray",
+  },
+  content: {
+    marginVertical: 5,
+  },
+});
 
 export default RestaurantInfo;
