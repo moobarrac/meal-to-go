fe42a5f5f44410bd506b4dfcae3638b64cc719a5
moobarrac <wahabajibola01@gmail.com>
2023-05-27 23:49:16 +0100

    added new fonts

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
+  const [latoLoaded] = useOswald({ Lato_400Regular });
+
+  if (!latoLoaded || oswaldLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
