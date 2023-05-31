0137a1bb8a455ac015d4f96b0d1516d2fbf5a9a6
moobarrac <wahabajibola01@gmail.com>
2023-05-31 21:22:38 +0100

    added flatlist

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
-import { SafeAreaView, StatusBar, View } from "react-native";
+import { FlatList, SafeAreaView, StatusBar, View } from "react-native";
 import { Searchbar } from "react-native-paper";
-import RestaurantInfo from "../components/restaurant-info.component";
+import RestaurantInfoCard from "../components/restaurant-info-card.component";
 import styled from "styled-components";
+import { SafeArea } from "../../../components/utility/safe-area";
 
 const RestaurantsScreen = () => {
-  const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px;`}
-  `;
-
   const SearchContainer = styled(View)`
     padding: ${(props) => props.theme.space[2]};
   `;
@@ -33,9 +29,31 @@ const RestaurantsScreen = () => {
             value={searchQuery}
           />
         </SearchContainer>
-        <RestaurantsListContainer>
+        {/* <RestaurantsListContainer>
           <RestaurantInfo />
-        </RestaurantsListContainer>
+        </RestaurantsListContainer> */}
+        <FlatList
+          data={[
+            { name: 1 },
+            { name: 2 },
+            { name: 3 },
+            { name: 4 },
+            { name: 5 },
+            { name: 6 },
+            { name: 7 },
+            { name: 8 },
+            { name: 9 },
+            { name: 10 },
+            { name: 11 },
+            { name: 12 },
+            { name: 13 },
+            { name: 14 },
+            { name: 15 },
+          ]}
+          renderItem={() => <RestaurantInfoCard />}
+          contentContainerStyle={{ padding: 16 }}
+          keyExtractor={(item) => item.name}
+        />
       </SafeArea>
     </>
   );
