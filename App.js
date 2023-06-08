493cdd58595b4f7c68145235baf76f9c90cf78a9
moobarrac <wahabajibola01@gmail.com>
2023-06-08 21:59:54 +0100

    added tab navigation

--- App.js
+++ App.js
@@ -8,8 +8,30 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text, View } from "react-native";
+import { SafeArea } from "./src/components/utility/safe-area";
+import { Ionicons } from "@expo/vector-icons";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+
+function SettingsScreen() {
+  return (
+    <SafeArea>
+      <Text>Settings!</Text>
+    </SafeArea>
+  );
+}
+function MapScreen() {
+  return (
+    <SafeArea>
+      <Text>Map!</Text>
+    </SafeArea>
+  );
+}
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [latoLoaded] = useLato({ Lato_400Regular });
 
@@ -17,10 +39,45 @@ export default function App() {
     return null;
   }
 
+  const tabBarIcons = {
+    Restaurants: {
+      focused: "md-restaurant",
+      unfocused: "md-restaurant-outline",
+    },
+    Settings: {
+      focused: "settings",
+      unfocused: "settings-outline",
+    },
+    Map: {
+      focused: "ios-map",
+      unfocused: "ios-map-outline",
+    },
+  };
+
+  const createScreenOptions = ({ route }) => ({
+    // eslint-disable-next-line react/no-unstable-nested-components
+    tabBarIcon: ({ focused, color, size }) => {
+      const iconName = focused
+        ? tabBarIcons[route.name].focused
+        : tabBarIcons[route.name].unfocused;
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+    tabBarActiveTintColor: "tomato",
+    tabBarInactiveTintColor: "gray",
+  });
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator screenOptions={createScreenOptions}>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={MapScreen} />
+              <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
